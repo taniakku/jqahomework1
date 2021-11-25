60af36ed877b29cef560c51beb5d022372f8c6c4
taniakko <tatiennekzm@gmail.com>
2021-11-25 11:06:55 +0300

    1st commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int bankAccount = 2_000_000_000;
+        int bankTransfer = 500_000_000;
+        int total = bankAccount + bankTransfer;
+        System.out.println(total);
+
+    }
+}
